ea8dc03b837fc97264ad27f7c89737e14dfe21e5
Wang Shouheng <shouheng2015@gmail.com>
2018-06-21 12:28:39 +0800

    modification

---
+++ client/app/src/main/java/cn/glority/letscorp/PalmApp.java
@@ -0,0 +1,49 @@
+package cn.glority.letscorp;
+
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+
+import com.crashlytics.android.Crashlytics;
+import com.facebook.stetho.Stetho;
+
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasActivityInjector;
+import dagger.android.support.HasSupportFragmentInjector;
+import io.fabric.sdk.android.Fabric;
+import me.shouheng.commons.BaseApplication;
+
+/**
+ * Created by WngShhng on 2018/6/21.*/
+public class PalmApp extends BaseApplication implements HasActivityInjector, HasSupportFragmentInjector {
+
+    @Inject
+    DispatchingAndroidInjector<Activity> activityInjector;
+
+    @Inject
+    DispatchingAndroidInjector<Fragment> fragmentInjector;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Fabric.with(this, new Crashlytics());
+
+        /* enable stetho only in debug mode */
+        if (BuildConfig.DEBUG) {
+            Stetho.initializeWithDefaults(this);
+        }
+    }
+
+    @Override
+    public AndroidInjector<Activity> activityInjector() {
+        return activityInjector;
+    }
+
+    @Override
+    public AndroidInjector<Fragment> supportFragmentInjector() {
+        return fragmentInjector;
+    }
+}
